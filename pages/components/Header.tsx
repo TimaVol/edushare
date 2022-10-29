e5f351ae497caa8d089fc476f4ef872be947538d
TimaVol <tima.voloshuk@gmail.com>
2022-10-29 19:12:13 +0300

    done

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,3 +1,45 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Logo } from '../../common/icons'
+import styles from '../../styles/components/Header.module.scss'
+import Container from './Container'
+
 export default function Header() {
-  return <header>header</header>
+  return (
+    <header>
+      <Container className={styles.Header}>
+        <Logo />
+        <nav>
+          <Link href="/" legacyBehavior>
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/">Profile</Link>
+          <Link href="/">Courses</Link>
+          <Link href="/">Contact</Link>
+        </nav>
+
+        <button className={styles.signIn}>Sign In</button>
+      </Container>
+      <Container className={styles.Intro}>
+        <div className={styles.textWrap}>
+          <h1>Discover best classes for the best learning</h1>
+          <p className={styles.subtitle}>
+            We designed Connections Academy to give students all across the
+            nation a tuition-free online public school that lets them learn.
+          </p>
+
+          <button>Get Started</button>
+
+          <p className={styles.hint}>
+            Not sure when to start?
+            <Link href={'/'}> Learn more</Link>
+          </p>
+        </div>
+
+        <div className={styles.imgWrap}>
+          <Image src={'/intro.png'} layout="fill" alt="intor banner" />
+        </div>
+      </Container>
+    </header>
+  )
 }
